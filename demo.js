a7794828d7f96ab6545783fab6195908a9625d9c
Harsh-919 <harshahir919@gamil.com>
2022-11-21 16:57:40 +0530

    ch

---
+++ demo.js
@@ -0,0 +1,8 @@
+var colors = ["red","green", "yellow", "black", "white", "indigo", "blue","violent", "skyblue" ]
+
+const [a,b,...args] =colors
+console.log(a)
+console.log(b)
+console.log(args)
+
+
